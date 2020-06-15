348cccf648d30355650a3f379f4e5f428b94e1a6
Facundo Rodriguez <facundo.rodriguez@fluxit.com.ar>
2020-06-15 14:49:46 -0300

    get database connection from environment property

--- server/config/config.js
+++ server/config/config.js
@@ -8,12 +8,8 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // Database
 
-let databaseUrl = 'mongodb+srv://administrator:fugZEbE0c5ZPlYsr@cluster0-yoadt.mongodb.net/cafe?retryWrites=true&w=majority'
-
 if(process.env.NODE_ENV === 'dev'){
-  databaseUrl = 'mongodb://localhost:27017/cafe';
+  process.env.DATABASE_URL  = 'mongodb://localhost:27017/cafe';
+}else{
+  process.env.DATABASE_URL = process.env.DATABASE_URL;
 }
-
-process.env.DATABASE_URL = databaseUrl;
-
- 
